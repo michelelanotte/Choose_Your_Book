82930369bfe5b509f2e20d609f6ee26474ea1dfe
Michele Lanotte <miki.mouse97@hotmail.it>
2016-03-31 20:57:19 +0200

    finish

--- reserve_book.php
+++ reserve_book.php
@@ -1,12 +1,25 @@
+<html>
+  <head>
+    
+	<?php
+	  @session_start();
+	  if ($_SESSION['logged_user'] != true)
+	  {
+	    header("location: login.php");
+	  } 
+    ?>
+	
+  </head>
+  
 <?php
   include("connect_database.php");
   @session_start();
   $title = $_GET['title'];
   $query = mysqli_query($conn, "SELECT * FROM books WHERE Title = '$title';");
   $book = mysqli_fetch_array($query);
-  if($book['Available']){
-	$query = mysqli_query($conn, "UPDATE books SET Available = 0 WHERE Title = '$title'");
-    header("location: receipt.php");
+  if($book['Available'] > 0){
+	$query = mysqli_query($conn, "UPDATE books SET Available = Available - 1 WHERE Title = '$title'");   
+	header("location: receipt.php?title=$title");
   }
   else{
 	header("location: view_books_user.php?failed=Libro non disponibile!!");
